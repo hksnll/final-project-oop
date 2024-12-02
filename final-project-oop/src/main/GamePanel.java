4e7c198b590eef71fcd4fc9953fe8a2b0a750132
hksnll <132461648+hksnll@users.noreply.github.com>
2024-12-02 17:25:53 +0800

    Delete final-project-oop directory

--- final-project-oop/src/main/GamePanel.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import javax.swing.*;
-
-public class GamePanel extends JPanel {
-    // SCREEN SETTINGS
-    final int originalTileSize = 16; // 16x16 tile
-    final int scale = 3;
-}
