6f3d598d845636877bf09315817d0263be82f82b
hksnll <jungyerin828@gmail.com>
2024-12-02 17:19:59 +0800

    hi

---
+++ final-project-oop/src/main/GamePanel.java
@@ -0,0 +1,9 @@
+package main;
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+}
